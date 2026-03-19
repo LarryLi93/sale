f655ebd426856aade0fc22e409efe00b362869fe
黄奕博 <16496635+huifeng233@user.noreply.gitee.com>
2026-03-19 12:48:03 +0800

    fix: 后端服务端口改为8012

--- server-admin.js
+++ server-admin.js
@@ -130,7 +130,7 @@ app.put('/api/products/:code', async (req, res) => {
   }
 });
 
-const PORT = process.env.ADMIN_API_PORT || 8013;
+const PORT = process.env.ADMIN_API_PORT || 8012;
 app.listen(PORT, () => {
   console.log(`Admin API server running on port ${PORT}`);
 });
